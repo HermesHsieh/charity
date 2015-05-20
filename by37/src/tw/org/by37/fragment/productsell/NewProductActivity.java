89807a17e7b84df4addd9470c0a83bd9a347d3de
Chun-Ta Huang <sky1987a@gmail.com>
2015-05-20 13:52:47 +0800

    no message

--- by37/src/tw/org/by37/fragment/productsell/NewProductActivity.java
+++ by37/src/tw/org/by37/fragment/productsell/NewProductActivity.java
@@ -7,6 +7,21 @@ import java.io.FileNotFoundException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 
 import tw.org.by37.R;
 import android.app.Activity;
@@ -18,12 +33,18 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +64,33 @@ public class NewProductActivity extends Activity {
         
     private String upLoadServerUri = null;
     private String imagepath=null;
+    
+    private EditText mProductNameEdittext;
+    private EditText mProductAmountEdittext;
+    private EditText mProductLovePointEdittext;
+    private EditText mProductDonateEdittext;
+    private EditText mProductDiscriptionEdittext;
+    private EditText mProductLocationEdittext;
+    private EditText mProductKeyWordEdittext;
+    
+    //Type
+    private Spinner mProductTypeSpinner;
+    private String[] types = {"3C電子", "家電", "衣服"};
+    private ArrayAdapter<String> mTypeAdapter;
+    
+    //Amount
+    private SeekBar mProductAmountSeekBar;
+    private TextView mProductAmountTextView;
+    
+    private String productName = "";
+    private String productType = "";
+    private String productAmount = "";
+    private String productLovePoint = "";
+    private String productDescription = "";
+    private String productTranspot = "";
+    private String productLocation = "";
+    private String productKeyWord = "";
+    
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +101,52 @@ public class NewProductActivity extends Activity {
 	}
 	
 	private void setView(){
+		
+		
+		
+		mProductTypeSpinner = (Spinner)findViewById(R.id.new_product_type_spinner);
+		mTypeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, types);
+		mProductTypeSpinner.setAdapter(mTypeAdapter);
+//		mProductTypeSpinner.setOnItemClickListener(new OnItemClickListener() {
+//
+//			@Override
+//			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+//					long arg3) {
+//				// TODO Auto-generated method stub
+//				productType = types[arg2];
+//				
+//			}
+//		});
+		
+		mProductAmountTextView = (TextView)findViewById(R.id.new_product_amount_text);
+		mProductAmountSeekBar = (SeekBar)findViewById(R.id.new_product_amount_seekbar);
+		mProductAmountSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				mProductAmountTextView.setText(""+progress);
+			}
+		});
+		
+		mProductNameEdittext = (EditText)findViewById(R.id.new_product_name_edittext);
+		mProductDiscriptionEdittext = (EditText)findViewById(R.id.new_product_discription_edittext);
+		mProductLocationEdittext = (EditText)findViewById(R.id.new_product_location_edittext);
+		mProductKeyWordEdittext = (EditText)findViewById(R.id.new_product_keyword_edittext);
+		
 		mSelectPictureBtn = (Button)findViewById(R.id.new_product_select_picture_btn);
 		mPictureImageView = (ImageView)findViewById(R.id.new_product_picture_imageview);
 		mSelectPictureBtn.setOnClickListener(new Button.OnClickListener() {
@@ -74,18 +168,25 @@ public class NewProductActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				dialog = ProgressDialog.show(NewProductActivity.this, "", "Uploading file...", true);
-	             messageText.setText("uploading started.....");
-	             new Thread(new Runnable() {
-	                 public void run() {
-	                      uploadFile(imagepath);
-	                 }
-	               }).start();     
+				
+				productName = mProductNameEdittext.getText().toString();
+				productAmount = mProductAmountTextView.getText().toString();
+				productDescription = mProductDiscriptionEdittext.getText().toString();
+				
+				new UpdateProductTask().execute();
+				
+//				dialog = ProgressDialog.show(NewProductActivity.this, "", "Uploading file...", true);
+//	             messageText.setText("uploading started.....");
+//	             new Thread(new Runnable() {
+//	                 public void run() {
+//	                      uploadFile(imagepath);
+//	                 }
+//	               }).start();     
 			}
 		});
 		
 		messageText  = (TextView)findViewById(R.id.messageText);
-		upLoadServerUri = "http://你的IP地址/UploadToServer.php";
+		upLoadServerUri = "http://charity.gopagoda.io/upload";
 	}
 	
 	 // 请求Gallery程序  
@@ -210,7 +311,7 @@ public class NewProductActivity extends Activity {
 				dos = new DataOutputStream(conn.getOutputStream());
 
 				dos.writeBytes(twoHyphens + boundary + lineEnd);
-				dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+				dos.writeBytes("Content-Disposition: form-data; name=\"image\";filename=\""
 						+ fileName + "\"" + lineEnd);
 
 				dos.writeBytes(lineEnd);
@@ -298,6 +399,85 @@ public class NewProductActivity extends Activity {
 			return serverResponseCode;
 
 		} // End else block
+	}
+	
+	class UpdateProductTask extends AsyncTask<Void, Void, Void>{
+		
+		
+		private ProgressDialog progDailog;
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			progDailog.dismiss();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			progDailog = new ProgressDialog(NewProductActivity.this);
+			progDailog.setMessage("上傳中...");
+			progDailog.setIndeterminate(false);
+			progDailog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			progDailog.setCancelable(true);
+			progDailog.show();
+			
+		}
+
+		@Override
+		protected Void doInBackground(Void... paramsss) {
+			// TODO Auto-generated method stub
+			String result = null;
+	        HttpClient client = new DefaultHttpClient();
+
+	        HttpPost post = new HttpPost("http://charity.gopagoda.io/addGoods");
+
+	        /*
+	         * Post運作傳送變數必須用NameValuePair[]陣列儲存
+	         */
+	        List<NameValuePair> params = new ArrayList<NameValuePair>();
+	        params.add(new BasicNameValuePair("name", productName));
+	        params.add(new BasicNameValuePair("description", productDescription));
+	        params.add(new BasicNameValuePair("quantity", productAmount));
+	        params.add(new BasicNameValuePair("image", imagepath));
+	        params.add(new BasicNameValuePair("user_id", "1"));
+	        params.add(new BasicNameValuePair("type", "1"));
+	       
+
+	        try {
+	                // setup multipart entity
+	                MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+	                for (int i = 0; i < params.size(); i++) {
+	                        // identify param type by Key
+	                        if (params.get(i).getName().equals("image")) {
+	                                entity.addPart(params.get(i).getName(), new FileBody(new File(params.get(i).getValue())));
+	                        } else {
+	                                entity.addPart(params.get(i).getName(), new StringBody(params.get(i).getValue(), Charset.forName("UTF-8")));
+	                        }
+	                }
+
+	                post.setEntity(entity);
+
+	                try {
+	                        // execute and get response
+	                        result = new String(client.execute(post, new BasicResponseHandler()).getBytes(), HTTP.UTF_8);
+
+	                        Log.i(TAG, "postUsers Result : " + result);
+	                } catch (Exception e) {
+	                        e.printStackTrace();
+	                        Log.d(TAG, e.toString());
+	                        Log.e(TAG, "postUsers Result Exception!");
+	                }
+	        } catch (Exception e) {
+	                e.printStackTrace();
+	                Log.d(TAG, e.toString());
+	                Log.e(TAG, "postUsers Exception");
+	        }
+			return null;
+		}
+		
 	}
 
 	
